c8f26c53c932ed571f7cc5628b8fafcf488a04de
Amnon Khen <amnon@ebi.ac.uk>
2025-10-20 15:23:25 +0100

    wip: solr population on k8s

--- bin/create-bulk-analytics-collection.sh
+++ bin/create-bulk-analytics-collection.sh
@@ -12,16 +12,20 @@ set -o pipefail
 HOST=${SOLR_HOST:-"localhost:8983"}
 SOLR_USER=${SOLR_USER:-"solr"}
 SOLR_PASS=${SOLR_PASS:-"SolrRocks"}
-SOLR_AUTH="-u ${SOLR_USER}:${SOLR_PASS}"
+SOLR_AUTH="-u \"${SOLR_USER}:${SOLR_PASS}\""
 COLLECTION=${SOLR_COLLECTION:-"bulk-analytics-v${SCHEMA_VERSION}"}
 
 NUM_SHARDS=${SOLR_NUM_SHARDS:-1}
 REPLICATION_FACTOR=${SOLR_REPLICATION_FACTOR:-1}
 # Default curl behavior: silent progress, show errors, fail on HTTP errors
-CURL_OPTS="\"${SOLR_AUTH}\" --fail"
+CURL_OPTS="${SOLR_AUTH} --fail"
 
 info "Deleting collection ${COLLECTION} on ${HOST} (ignore if not exists)"
 curl ${CURL_OPTS} "http://${HOST}/solr/admin/collections?action=DELETE&name=$COLLECTION&numShards=$NUM_SHARD&replicationFactor=$REPLICATION_FACTOR"
+
+info "Creating collection ${COLLECTION} on ${HOST}"
+curl ${CURL_OPTS}"http://${HOST}/solr/admin/collections?action=CREATE&name=$COLLECTION&numShards=$NUM_SHARDS&replicationFactor=$REPLICATION_FACTOR"
+success "Collection ensured: ${COLLECTION}"
 
 info "Creating collection ${COLLECTION} on ${HOST}"
 curl ${CURL_OPTS}"http://${HOST}/solr/admin/collections?action=CREATE&name=$COLLECTION&numShards=$NUM_SHARDS&replicationFactor=$REPLICATION_FACTOR"
@@ -32,11 +36,14 @@ success "Collection ensured: ${COLLECTION}"
 
 info "Disabling auto-commit and soft auto-commit in ${COLLECTION}"
 curl ${CURL_OPTS} "http://${HOST}/solr/${COLLECTION}/config" -H 'Content-type:application/json' -d '{
+info "Disabling auto-commit and soft auto-commit in ${COLLECTION}"
+curl ${CURL_OPTS} "http://${HOST}/solr/${COLLECTION}/config" -H 'Content-type:application/json' -d '{
   "set-property": {
     "updateHandler.autoCommit.maxTime":-1
   }
 }'
 
+curl ${CURL_OPTS} "http://${HOST}/solr/${COLLECTION}/config" -H 'Content-type:application/json' -d '{
 curl ${CURL_OPTS} "http://${HOST}/solr/${COLLECTION}/config" -H 'Content-type:application/json' -d '{
   "set-property": {
     "updateHandler.autoCommit.maxDocs":-1
@@ -44,16 +51,25 @@ curl ${CURL_OPTS} "http://${HOST}/solr/${COLLECTION}/config" -H 'Content-type:ap
 }'
 
 curl ${CURL_OPTS} "http://${HOST}/solr/${COLLECTION}/config" -H 'Content-type:application/json' -d '{
+curl ${CURL_OPTS} "http://${HOST}/solr/${COLLECTION}/config" -H 'Content-type:application/json' -d '{
   "set-property": {
     "updateHandler.autoSoftCommit.maxTime":-1
   }
 }'
 
 curl ${CURL_OPTS} "http://${HOST}/solr/${COLLECTION}/config" -H 'Content-type:application/json' -d '{
+curl ${CURL_OPTS} "http://${HOST}/solr/${COLLECTION}/config" -H 'Content-type:application/json' -d '{
   "set-property": {
     "updateHandler.autoSoftCommit.maxDocs":-1
   }
 }'
+#############################################################################################
+
+info "Creating alias 'bulk-analytics' -> ${COLLECTION}"
+printf "\n\nAliasing base collection atlas-bulk to latest iteration ${COLLECTION}\n"
+curl ${CURL_OPTS} "http://${HOST}/solr/admin/collections?action=CREATEALIAS&name=bulk-analytics&collections=${COLLECTION}"
+
+success "created collection ${COLLECTION}"
 #############################################################################################
 
 info "Creating alias 'bulk-analytics' -> ${COLLECTION}"
